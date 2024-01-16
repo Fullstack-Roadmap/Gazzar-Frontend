e9222b77b2904997672fdad2feeb055c08291422
Okoye <okoyedivine145@gmail.com>
2024-01-16 22:58:32 +0100

    Put basic elements, no major styling done

--- src/components/LandingMidSection.jsx
+++ src/components/LandingMidSection.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const LandingMidSection = () => {
   return (
-    <div>
-      <h1>Helping you level up your business.</h1>
-      <p>
+    <div className="flex flex-col items-center justify-center bg-blue w-screen h-[1200px] text-white gap-10">
+      <header className="text-lg bg-slate-400 py-2 px-5 m-2 rounded-full"> OUR GOAL IS SIMPLE</header>
+      <h1 className="text-3xl font-bold">Helping you level up your business.</h1>
+      <p className="text-sm">
         With different tools aimed at helping you scale up, Gazzar’s the best
         match for helping you with that!
       </p>
