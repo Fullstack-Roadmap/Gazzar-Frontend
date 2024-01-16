560213d6f0d03ca7673b1868df42456ec0cda418
Okoye <okoyedivine145@gmail.com>
2024-01-16 22:51:24 +0100

    Craeted Landing Page midsection

---
+++ src/components/LandingMidSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LandingMidSection = () => {
+  return (
+    <div>
+      <h1>Helping you level up your business.</h1>
+      <p>
+        With different tools aimed at helping you scale up, Gazzar’s the best
+        match for helping you with that!
+      </p>
+    </div>
+  );
+};
+
+export default LandingMidSection;
--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import LandingMidSection from "../components/LandingMidSection";
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col items-center h-[3600px] font-semibold text-xl mx-20">
       <Navbar />
       <Hero />
+      <LandingMidSection/>
     </div>
   );
 };
