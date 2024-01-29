2b997e583d666da6b302d9b839b967796879adbd
Okoye <okoyedivine145@gmail.com>
2024-01-29 16:26:13 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import LandingPage from "./routes/LandingPage";
-
 const App = () => {
-  return (
-    <>
-    </>
-  );
+  return <></>;
 };
 
 const Root = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen gap-[20vw] text-blue font-bold text-2xl">
-        <Link to="/register"> Register Business </Link>
-        <Link to="/signin"> Sign In </Link>
-      </div>
-
-      <div>
-        <Outlet />
-      </div>
+      <Outlet />
     </>
   );
 };
--- src/routes/StoreFront.jsx
+++ src/routes/StoreFront.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
 const StoreFront = () => {
   return (
-    <div>StoreFront</div>
-  )
-}
+    <>
+      <nav className="flex justify-between items-center mx-40 my-8">
+        <section className="flex justify-center aspect-square w-12 rounded-full bg-gray"></section>
+        <ul className="flex justify-center gap-5">
+          <li>Home</li>
+          <li>Products</li>
+          <li>Categories</li>
+        </ul>
+        <section className="flex justify-center items-center gap-3">
+          <div className="bg-gray flex h-fit justify-center items-center py-3 px-4 rounded-lg gap-3">
+            <span className="w-5 bg-blue rounded-full aspect-square"></span>
+            <input type="text" placeholder="search or type" className="bg-transparent focus:outline-none" />
+          </div>
+          <div className="rounded-full h-[3.25rem] aspect-square flex justify-center items-center border-[1px] border-gray"></div>
+        </section>
+      </nav>
+    </>
+  );
+};
 
-export default StoreFront
+export default StoreFront;
