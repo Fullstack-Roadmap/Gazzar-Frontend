b7e3f2362cc9ea41300a778946a7e4f710e36615
Meekaylah <kaylahusanga@gmail.com>
2024-01-31 14:39:03 +0100

    fix: added hamburger icon for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,36 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
   const blue = "#083167";
   const lightBlue = "#1B73D326";
 
   return (
-    <nav className="sticky sm:hidden md:hidden lg:flex items-center mt-16 py-6 px-8 rounded-3xl h-fit w-full shadow-custom justify-between text-lg mx-20">
-      <header className="text-2xl font-extrabold flex justify-center items-center gap-2 h-fit">
+    <nav className="sticky flex items-center mt-16 py-6 px-8 rounded-3xl h-fit w-full shadow-custom justify-between text-lg mx-20">
+      <header className="text-2xl font-extrabold flex items-center gap-2 h-fit">
         <span className="mr-1 rounded-md aspect-square h-8 bg-[#FFB722]"></span>
         <p>Gazzar.</p>
       </header>
-      <section className="flex gap-16 items-center mx-2">
+      <section className="lg:flex gap-16 items-center mx-2 hidden">
         <p>Features</p>
         <p>Company</p>
         <p>Pricing</p>
         <p>Learn</p>
       </section>
-      <section className="flex gap-4 items-center font-semibold">
-        <div>
+      <section className="gap-4 items-center font-semibold flex justify-end">
+        <div className="hidden lg:flex">
           <Link to="signin">
             <Button text="Login" buttonColor={lightBlue} textBlue={true} />
           </Link>
         </div>
-        <div>
+        <div className="hidden lg:flex">
           <Link to="register">
             <Button text="Get Started" buttonColor={blue} />
           </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <FaBars />
         </div>
       </section>
     </nav>
