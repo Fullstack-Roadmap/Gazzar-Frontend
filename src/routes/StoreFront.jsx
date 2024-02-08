45c5cff26f0614c73d3120d8088355ec87898ba6
Okoye <okoyedivine145@gmail.com>
2024-02-08 03:32:43 +0100

    Navbar fully responsive

--- src/routes/StoreFront.jsx
+++ src/routes/StoreFront.jsx
@@ -4,14 +4,36 @@ import profilePic from "../assets/profilePic.png";
 import ellipses from "../assets/svgs/ellipses.svg";
 import search from "../assets/svgs/search.svg";
 import Product from "../components/Product";
+import { useState } from "react";
 
 const StoreFront = () => {
+  const [menuState, setMenuState] = useState(false);
+  const toggleMenu = () => {
+    setMenuState(!menuState);
+  };
+  const exitMenu = () => {
+    setMenuState(false);
+  };
   var products = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <body className="overflow-hidden">
+      {!menuState && (
+        <li className="z-30 w-screen h-screen backdrop-blur-sm absolute lg:hidden"></li>
+      )}
       <nav className="flex justify-between items-center lg:mx-36 md:mx-20 my-7 font-semibold text-black/50 sm:justify-around">
         <img src={profilePic} className="w-14" alt="profilePic" />
-        <ul className="sm:flex-col md:flex-col lg:flex-row flex lg:static justify-center items-center lg:gap-7 absolute bg-white top-0 right-0 p-10 w-1/2 lg:h-fit h-screen z-30 lg:text-base text-3xl gap-16">
+
+        <ul
+          className={`sm:flex-col md:flex-col lg:flex-row fixed lg:static flex justify-center items-center lg:gap-7 lg:bg-transparent bg-slate-200 top-0 right-0 p-10 w-3/4 lg:h-fit h-screen z-30 lg:text-base text-3xl sm:text-xl gap-16 transition-all duration-300 ease-in-out ${
+            menuState && "translate-x-[80vw]"
+          }`}
+        >
+          <li
+            className="cursor-pointer p-5 rounded-full top-5 right-5 absolute text-blue"
+            onClick={toggleMenu}
+          >
+            X
+          </li>
           <li className="text-blue font-bold cursor-pointer">Home</li>
           <li className="cursor-pointer">Products</li>
           <li className="cursor-pointer">Categories</li>
@@ -25,8 +47,11 @@ const StoreFront = () => {
               className="bg-transparent w-[150px] focus:outline-none placeholder:text-sm text-black"
             />
           </div>
-          <div className="rounded-full h-[3.25rem] aspect-square flex justify-center items-center border-[1px] border-gray">
-            <img src={ellipses} alt="" />
+          <div
+            className="rounded-full h-[3.25rem] aspect-square flex justify-center items-center border-[1px] border-gray cursor-pointer"
+            onClick={toggleMenu}
+          >
+            <img src={ellipses} alt="dots" />
           </div>
         </section>
       </nav>
