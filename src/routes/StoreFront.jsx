c9c2c288b8b9f495c368f53511a0e4d8231d923f
Okoye <okoyedivine145@gmail.com>
2024-02-08 15:01:22 +0100

    Finishing touches

--- src/routes/StoreFront.jsx
+++ src/routes/StoreFront.jsx
@@ -20,16 +20,21 @@ const StoreFront = () => {
           onClick={toggleMenu}
         ></li>
       )}
-      <nav className="flex justify-between items-center lg:mx-36 md:mx-20 my-7 font-semibold text-black/50 sm:justify-around">
-        <img src={profilePic} className="w-14" alt="profilePic" />
+      <nav className="flex justify-between items-center lg:mx-36 md:mx-20 sm:my-6 md:my-6 lg:my-0 font-semibold text-black/50 sm:justify-around">
+        <img
+          src={profilePic}
+          className="w-14  cursor-pointer"
+          alt="profilePic"
+        />
 
         <ul
           className={`sm:flex-col md:flex-col lg:flex-row fixed lg:static flex justify-center items-center lg:gap-7 lg:bg-transparent bg-slate-200 top-0 right-0 p-10 w-3/4 lg:h-fit h-screen z-30 lg:text-base text-3xl sm:text-xl gap-16 transition-all duration-300 ease-in-out ${
-            !menuState && "translate-x-[80vw]"
+            !menuState &&
+            "sm:translate-x-[80vw] md:translate-x-[80vw] lg:translate-x-0"
           }`}
         >
           <li
-            className="cursor-pointer p-5 rounded-full top-5 right-5 absolute text-blue"
+            className="lg:hidden cursor-pointer p-5 rounded-full top-5 right-5 absolute text-blue"
             onClick={toggleMenu}
           >
             X
@@ -40,7 +45,7 @@ const StoreFront = () => {
         </ul>
         <section className="flex justify-center items-center gap-3">
           <div className="bg-[#DFDFDF]/30 flex h-fit justify-start items-center py-3 px-5 rounded-lg gap-3 border-[1px] border-black/10 w-[300px] sm:w-[200px]">
-            <img src={search} alt="search" className="w-5" />
+            <img src={search} alt="search" className="w-5 cursor-pointer" />
             <input
               type="text"
               placeholder="Search or type"
@@ -72,22 +77,22 @@ const StoreFront = () => {
         </div>
       </section>
       <nav>
-        <ul className="justify-start flex-wrap flex lg:mx-36 md:mx-20 sm:mx-10 mt-[4.5rem] gap-3 text-xs font-medium">
-          <li className="bg-[#083167] px-6 py-4 text-white rounded-lg">
+        <ul className="justify-start flex-wrap flex lg:mx-36 md:mx-20 sm:mx-10 sm:mt-[4.5rem] md:mt-12 gap-3 text-xs font-medium">
+          <li className="bg-[#083167] px-6 py-4 text-white rounded-lg cursor-pointer">
             All (74)
           </li>
-          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg">
+          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg cursor-pointer">
             Shoes
           </li>
-          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg">
+          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg cursor-pointer">
             Clothes
           </li>
-          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg">
+          <li className="bg-[#DFDFDF] px-6 py-4 text-[6D6D6D] rounded-lg cursor-pointer">
             Bags
           </li>
         </ul>
       </nav>
-      <ul className="flex flex-wrap justify-center items-center gap-10 w-full mt-12">
+      <ul className="flex flex-wrap justify-center items-center gap-10 w-full mt-12 px-10">
         {products.map(() => (
           <li className="shadow-custom">
             <Product />
