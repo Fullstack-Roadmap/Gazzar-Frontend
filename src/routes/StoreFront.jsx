ad44e5ec96969bb873c87ba133929a03d4ac7433
Okoye <okoyedivine145@gmail.com>
2024-02-08 02:56:58 +0100

    Navbar responsiveness for storefront

--- src/routes/StoreFront.jsx
+++ src/routes/StoreFront.jsx
@@ -6,19 +6,19 @@ import search from "../assets/svgs/search.svg";
 import Product from "../components/Product";
 
 const StoreFront = () => {
-  var products = [1,2,3,4,5,6,7,8];
+  var products = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <body className="overflow-hidden">
-      <nav className="flex justify-between items-center mx-36 my-7 font-semibold text-black/50">
-        <img src={profilePic} className="w-14" alt="" />
-        <ul className="flex justify-center gap-7 ">
+      <nav className="flex justify-between items-center lg:mx-36 md:mx-20 my-7 font-semibold text-black/50 sm:justify-around">
+        <img src={profilePic} className="w-14" alt="profilePic" />
+        <ul className="sm:flex-col md:flex-col lg:flex-row flex lg:static justify-center items-center lg:gap-7 absolute bg-white top-0 right-0 p-10 w-1/2 lg:h-fit h-screen z-30 lg:text-base text-3xl gap-16">
           <li className="text-blue font-bold cursor-pointer">Home</li>
           <li className="cursor-pointer">Products</li>
           <li className="cursor-pointer">Categories</li>
         </ul>
         <section className="flex justify-center items-center gap-3">
-          <div className="bg-[#DFDFDF]/30 flex h-fit justify-start items-center py-3 px-5 rounded-lg gap-3 border-[1px] border-black/10 w-[300px]">
-            <img src={search} alt="" className="w-5" />
+          <div className="bg-[#DFDFDF]/30 flex h-fit justify-start items-center py-3 px-5 rounded-lg gap-3 border-[1px] border-black/10 w-[300px] sm:w-[200px]">
+            <img src={search} alt="search" className="w-5" />
             <input
               type="text"
               placeholder="Search or type"
@@ -30,22 +30,24 @@ const StoreFront = () => {
           </div>
         </section>
       </nav>
-      <section className="flex flex-col justify-center items-start text-white h-[520px] overflow-hidden relative gap-10">
-        <p className="mx-36 z-20 text-6xl font-extrabold w-[53%] leading-[1.4]">
+      <section className="flex flex-col justify-center items-start text-white h-[520px] overflow-hidden relative gap-10 sm:gap-5">
+        <p className="lg:mx-36 md:mx-20 sm:mx-10 z-20 lg:text-6xl md:text-5xl sm:text-3xl font-extrabold w-[53%] sm:w-3/4 leading-[1.4]">
           I sell shoes, clothes, bags and other clothing accessories.
         </p>
-        <div className="mx-36 z-20">
+        <div className="lg:mx-36 md:mx-20 sm:mx-10 z-20">
           <Button
             text="Shop Now"
             borderWhite={true}
-            customClasses="px-16 py-4 text-xl"
+            customClasses="md:px-8 md:py-2 md:text-lg lg:px-16 lg:py-4 lg:text-xl"
           />
         </div>
 
-        <img src={storeFrontHeroPic} alt="" className="z-10 absolute" />
+        <div className="flex justify-center z-10 absolute bg-green-500 sm:h-80 md:h-[30rem] lg:h-fit">
+          <img src={storeFrontHeroPic} alt="heroPic" className="object-cover" />
+        </div>
       </section>
       <nav>
-        <ul className="flex mx-36 mt-[4.5rem] gap-3 text-xs font-medium">
+        <ul className="justify-start flex-wrap flex lg:mx-36 md:mx-20 sm:mx-10 mt-[4.5rem] gap-3 text-xs font-medium">
           <li className="bg-[#083167] px-6 py-4 text-white rounded-lg">
             All (74)
           </li>
@@ -60,7 +62,7 @@ const StoreFront = () => {
           </li>
         </ul>
       </nav>
-      <ul className="flex flex-wrap gap-10 w-full mx-36 mt-12">
+      <ul className="flex flex-wrap justify-center items-center gap-10 w-full mt-12">
         {products.map(() => (
           <li className="shadow-custom">
             <Product />
