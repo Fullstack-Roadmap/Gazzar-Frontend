30a700e13267ddba672039294da198a45d0c7790
Meekaylah <kaylahusanga@gmail.com>
2024-02-14 00:59:05 +0100

    Created CartProductCard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,8 @@ export default {
       colors: {
         blue: "#083167",
         gray: "#DFDFDF",
-        outlinegray: "#D9D9D9"
+        outlinegray: "#D9D9D9",
+        navyblue: "#CED6E1"
       },
     },
     plugins: [],
