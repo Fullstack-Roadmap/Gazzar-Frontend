a38ae610ba6712033b94807cea68b1e0e37660a7
Okoye <okoyedivine145@gmail.com>
2024-02-16 01:32:46 +0100

    Endpoint fo invalid OTP requests

--- src/routes/OTPVerification.jsx
+++ src/routes/OTPVerification.jsx
@@ -36,7 +36,7 @@ const OTPVerification = () => {
         navigate("/otp-verified");
       } catch (error) {
         console.log(error);
-        setOtpError(JSON.stringify(error));
+        setOtpError("Invalid OTP");
       }
     }
 
