52775308c5cc531b4f32cdb38191aeeb583490a5
Meekaylah <kaylahusanga@gmail.com>
2024-02-23 00:49:19 +0100

    Landing page sidebar added

---
+++ src/components/SidebarData.js
@@ -0,0 +1,22 @@
+export const SidebarData = [
+    {
+        title: "Features",
+        path: "/",
+        cName: "nav-text",
+    },
+    {
+        title: "Company",
+        path: "/",
+        cName: "nav-text",
+    },
+    {
+        title: "Pricing",
+        path: "/",
+        cName: "nav-text",
+    },
+    {
+        title: "Learn",
+        path: "/",
+        cName: "nav-text",
+    },
+]
