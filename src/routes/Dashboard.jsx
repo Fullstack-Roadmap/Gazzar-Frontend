4fe6ae339b09f52a63cd199e6375b805b8168a64
Okoye <okoyedivine145@gmail.com>
2024-02-24 21:50:20 +0100

    Minor UX additions

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -13,6 +13,7 @@ import store from "../assets/svgs/store.svg";
 import profile from "../assets/svgs/profile.svg";
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ const Dashboard = () => {
   const setNavActive = (navItem) => {
     setActive(navItem);
   };
+  const [menuOpen, setMenuOpen] = useState(false);
   const navItems = [
     { name: "Overview", svg: overview },
     { name: "Statistics", svg: statistics },
@@ -30,9 +32,27 @@ const Dashboard = () => {
     { name: "Store", svg: store },
     { name: "Profile", svg: profile },
   ];
+  const capitalize = (word) => {
+    const firstLetter = word.charAt(0);
+    const firstLetterCap = firstLetter.toUpperCase();
+    const remainingLetters = word.slice(1);
+    const capitalizedWord = firstLetterCap + remainingLetters;
+    return capitalizedWord;
+  };
   return (
     <div className="flex h-full">
-      <section className="h-full border-r-[1px] border-b-[1px] rounded-md border-gray">
+      {menuOpen && (
+        <div
+          className="absolute z-10 w-screen h-screen"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+      )}
+      <section
+        on
+        className={`lg:-translate-x-0 h-full bg-white border-r-[1px] border-b-[1px] rounded-md border-gray z-20 transition-all delay-300 duration-[400ms] ease-in-out ${
+          menuOpen ? "" : "sm:-translate-x-full md:-translate-x-full"
+        }`}
+      >
         <header
           className="text-[1.375rem] font-extrabold flex items-center gap-2 mx-16 mb-14 mt-16 -translate-x-5 cursor-pointer"
           onClick={() => navigate("/storefront")}
@@ -40,7 +60,14 @@ const Dashboard = () => {
           <span className="mr-1 rounded-md aspect-square h-8 bg-[#FFB722] "></span>
           <p>Gazzar.</p>
         </header>
-        <ul className="flex flex-col gap-1 items-start h-full px-4">
+        <ul
+          className={`flex flex-col gap-1 items-start h-full px-4 transition-all delay-200 duration-300 ease-in-out ${
+            menuOpen
+              ? "lg:translate-x-0"
+              : "sm:-translate-x-full md:-translate-x-full lg:translate-x-0"
+          }`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
           {navItems.map((item, index) => (
             <li
               key={index}
@@ -52,34 +79,45 @@ const Dashboard = () => {
               onClick={() => setNavActive(item.name)}
             >
               <div
-                className={`absolute left-0 rounded-full bg-blue w-1 flex transition-all duration-200 ease-in-out ${
+                className={`absolute left-0 rounded-full bg-blue w-1 flex transition-all duration-300 ease-in-out ${
                   active === item.name ? "h-3/4" : "h-0"
                 }`}
               />
-              <img src={item.svg} alt="" className="" />
+              <img src={item.svg} alt={item.name} />
               {item.name}
             </li>
           ))}
         </ul>
       </section>
-      <section className="w-full">
-        <nav className="flex justify-between items-center border-b-[1px] border-gray px-10 py-6">
-          <span className="font-extrabold text-3xl">Overview</span>
-          <nav className="flex gap-4">
-            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer">
-              <img src={book} alt="dots" />
+      <section className="w-full sm:absolute md:absolute lg:static">
+        <nav className="flex justify-between items-center border-b-[1px] border-gray px-10 sm:pr-6 py-6">
+          <IoIosArrowForward
+            className="z-20 absolute lg:hidden -translate-x-6 scale-125 cursor-pointer"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          />
+          <span
+            className="font-extrabold text-3xl sm:text-xl cursor-pointer"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            {capitalize(active)}
+          </span>
+          <nav className="flex gap-4 sm:gap-2">
+            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer sm:h-8">
+              <img src={book} alt="dots" className="sm:scale-[65%]" />
             </div>
-            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer">
-              <img src={link} alt="dots" />
+            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer sm:h-8">
+              <img src={link} alt="dots" className="sm:scale-[65%]" />
             </div>
-            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer">
-              <img src={bell} alt="dots" />
+            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer sm:h-8">
+              <img src={bell} alt="dots" className="sm:scale-[65%]" />
             </div>
-            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer">
-              <img src={shop} alt="dots" />
+            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer sm:h-8">
+              <img src={shop} alt="dots" className="sm:scale-[65%]" />
             </div>
-            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer">
-              <img src={ellipses} alt="dots" />
+            <div className="rounded-full h-[3.5rem] aspect-square flex justify-center items-center border-2 border-gray cursor-pointer sm:h-8">
+              <img src={ellipses} alt="dots" className="sm:scale-[65%]" />
             </div>
           </nav>
         </nav>
