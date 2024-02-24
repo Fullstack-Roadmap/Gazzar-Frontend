1d6db99a215dd57ab05187923a4d84c59d61b22f
Meekaylah <kaylahusanga@gmail.com>
2024-02-24 01:58:24 +0100

    Cart components and page

--- src/components/CartProductCard.jsx
+++ src/components/CartProductCard.jsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
+import Shoe2 from "../assets/shoe2.png";
+import deleteIcon from "../assets/svgs/deleteIcon.svg";
 
 
 const CartProductCard = () => {
+
+    const[count, setCount] = useState(1);
+
+    const add = () => {
+        setCount(count + 1);
+    };
+    const subtract = () => {
+        if (count > 1){
+            setCount(count - 1);
+        }
+    };
+
   return (
-    <div className="h-fit px-12 pb-6 rounded-xl border border-outlinegray flex">
+    <div className="h-fit pl-5 pr-9 pb-6 rounded-xl border border-outlinegray flex">
         <div className="flex flex-col gap-5 w-1/4 mt-6">
-            <div className="w-[6.375rem] h-[6.25rem] border text-center">Image</div>
-            <span>Delete</span>
+            <img className="w-[6.375rem] h-[6.25rem]" src={Shoe2} alt="Shoe"/>
+            <img className="w-[2rem] h-[2rem]" src={deleteIcon} alt="delete" />
         </div>
         <div className="flex flex-col w-2/4 gap-3 mt-6">
-            <span>Airforce 1's</span>
-            <div className="flex gap-5">
-                <span>Brand</span>
-                <span>Variations</span>
+            <span className="text-2xl font-black">Airforce 1's</span>
+            <div className="flex gap-4">
+                <span className="text-base text-[#919191]">Brand: <span className="text-black">Nike</span></span>
+                <span className="text-base text-[#919191]">Variations: <span className="text-black">Size 47</span></span>
             </div>
         </div>
-        <div className="flex flex-col gap-3 w-1/4 mt-6">
-            <span>N30,000</span>
-            <span>Discount</span>
+        <div className="flex gap-16 flex-col w-1/4 mt-6 items-end">
+            <div className="flex flex-col items-end">
+                <span className="text-[1.125rem] font-bold">N30,000</span>
+                <span className="mt-2 text-[#919191] text-[0.875rem]">Discount: <span className="text-black">20%</span></span>
+            </div>
+            <div className="flex gap-4">
+                <button className="w-[1.5625rem] h-[1.5625rem] text-white rounded bg-[#083167]" onClick={subtract}>
+                    <span>-</span>
+                </button>
+                <span>{count}</span>
+                <button className="w-[1.5625rem] h-[1.5625rem] text-white rounded bg-[#083167]" onClick={add}>
+                    <span>+</span>
+                </button>
+            </div>
         </div>
     </div>
   )
--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,19 +1,46 @@
 import CartProductCard from "../components/CartProductCard";
+import UserNavbar from "../components/UserNavbar";
+import { Link } from "react-router-dom";
+import { SPImagesData } from "../components/SPImagesData";
+import Button from "../components/Button";
 
 const Cart = () => {
   return (
-      <div className="h-fit mt-16 px-12 pb-16 rounded-xl border border-outlinegray mx-40">
+    <>
+    <UserNavbar></UserNavbar>
+      <div className="h-fit mt-14 px-8 pb-16 rounded-xl border border-outlinegray mx-40">
         <h3 className="text-4xl font-extrabold mt-4 ml-12">Cart</h3>
-        <div className="flex gap-12">
+        <div className="flex gap-8">
           <div className="flex flex-col w-2/3 gap-6 mt-12">
             <CartProductCard />
             <CartProductCard />
           </div>
-          <div className="w-1/3 border">
-
+          <div className="w-1/3 h-[14.625rem] mt-10 rounded-xl bg-[#F7F7F7] border border-outlinegray px-6 pt-4">
+            <div className="flex flex-col gap-4 w-full border-b border-outlinegray pb-3">
+              <h4 className="text-xl font-bold tracking-tight">Summary</h4>
+              <span className=" text-base font-bold">3 items</span>
+            </div>
+            <div className="flex mt-3 justify-between">
+              <span>Subtotal</span>
+              <span className="text-base font-bold tracking-tight">N60,000</span>
+            </div>
+            <Button text="Checkout" buttonColor="#083167"customClasses={"w-full mt-6 flex justify-center items-center"}/>
           </div>
         </div>
       </div>
+      <div className="h-fit my-12 p-7 rounded-xl border border-outlinegray mx-40">
+        <h3 className="text-xl font-bold">Similar Products</h3>
+        <div className="flex gap-8">
+          {SPImagesData.map((item, index) => {
+            return (
+              <Link key={index} className="mt-6 w-[11rem] h-[10rem]" to={item.path}>
+                  <img className="w-full h-full" src={item.image} alt={item.name} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
