1471d03aa9aab336851bfaa48637ce712291dfdd
Okoye <okoyedivine145@gmail.com>
2024-02-24 19:38:32 +0100

    More Navitem active state changes

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -13,10 +13,10 @@ import store from "../assets/svgs/store.svg";
 import profile from "../assets/svgs/profile.svg";
 import { useState } from "react";
 const Dashboard = () => {
-  const [active, setActive] = useState("overview")
+  const [active, setActive] = useState("overview");
   const setNavActive = (navItem) => {
-    setActive(navItem)
-  }
+    setActive(navItem);
+  };
   return (
     <div className="flex h-full">
       <section className="h-full border-r-[1px] border-b-[1px] rounded-md border-gray">
@@ -25,58 +25,67 @@ const Dashboard = () => {
           <p>Gazzar.</p>
         </header>
         <ul className="flex flex-col gap-1 items-start h-full px-4">
-          <li className="relative flex items-center gap-2 pl-7 py-4 w-full text-blue text-sm font-bold bg-slate-200 rounded-lg">
-            <div
-              className="absolute left-0 rounded-full h-3/4 bg-blue w-1 flex"
-              onClick={() => setNavActive("overview")}
-            ></div>
+          <li
+            className={`relative flex items-center gap-2 pl-7 py-4 w-full text-blue text-sm font-bold bg-slate-200 rounded-lg`}
+            onClick={() => setNavActive("overview")}
+          >
+            <div className="absolute left-0 rounded-full h-3/4 bg-blue w-1 flex" />
             <img src={overview} alt="" />
             Overview
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-4 w-full rounded-lg text-sm  ${
+              active === "statistics"
+                ? "font-bold bg-slate-200 text-blue"
+                : "font-semibold text-black/70"
+            }`}
             onClick={() => setNavActive("statistics")}
           >
-            <img src={statistics} alt="" />
+            <div
+              className={`absolute left-0 rounded-full bg-blue w-1 flex transition-all duration-200 ease-in-out ${
+                active === "statistics" ? "h-3/4" : "h-0"
+              }`}
+            />
+            <img src={statistics} alt="" className="" />
             Statistics
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("products")}
           >
             <img src={products} alt="" />
             Products
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("customers")}
           >
             <img src={customers} alt="" />
             Customers
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("feedback")}
           >
             <img src={feedback} alt="" />
             Feedback
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("orders")}
           >
             <img src={orders} alt="" />
             Orders
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("store")}
           >
             <img src={store} alt="" />
             Store
           </li>
           <li
-            className="relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold"
+            className={`relative flex items-center gap-2 pl-7 py-5 w-full rounded-lg text-black/70 text-sm font-semibold`}
             onClick={() => setNavActive("profile")}
           >
             <img src={profile} alt="" />
