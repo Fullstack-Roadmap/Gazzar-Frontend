9b927305aff2886434ecf7042ea21a2f37de48b5
Okoye <okoyedivine145@gmail.com>
2024-02-27 23:21:55 +0100

    overveiw cto complete

--- src/routes/Overview.jsx
+++ src/routes/Overview.jsx
@@ -1,12 +1,12 @@
 const Overview = () => {
   return (
     <>
-      <div className="flex justify-between items-center px-5 py-3 m-5 bg-blue/30 rounded-lg">
-        <div className="flex gap-3">
+      <div className="flex justify-between items-center px-5 py-3 m-6 bg-blue/20 rounded-xl border-2 border-blue/40">
+        <div className="flex gap-3 font-semibold">
           <img src="" alt="checkmark" />
           <div className="text-blue">Youre a few steps away from finishing</div>
         </div>
-        <button className="bg-blue text-white p-5 rounded-lg">
+        <button className="bg-blue text-white py-4 px-10 rounded-lg font-semibold">
           Complete account setup
         </button>
       </div>
