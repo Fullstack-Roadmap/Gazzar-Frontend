8ed95fb1542945a169a3c425c1b992bc8f80eb5c
Okoye <okoyedivine145@gmail.com>
2024-02-27 23:09:47 +0100

    overview layout made

--- src/routes/Overview.jsx
+++ src/routes/Overview.jsx
@@ -1,10 +1,20 @@
 const Overview = () => {
   return (
     <>
-      <div className="p-10">
+      <div className="flex p-5 bg-blue/50 rounded-lg">
         <img src="" alt="checkmark" />
         <div className="text-blue">Youre a few steps away from finishing</div>
-        <button className="bg-blue text-white p-5">Complete account setup</button>
+        <button className="bg-blue text-white p-5 rounded-lg">
+          Complete account setup
+        </button>
+      </div>
+      <div className="m-10">
+        <ul></ul>
+        <section>
+          <div>Amount made</div>
+          <div>Top selling products</div>
+        </section>
+        <section>pending orders</section>
       </div>
     </>
   );
